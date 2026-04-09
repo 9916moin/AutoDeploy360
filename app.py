cf99fc51e74c05c0d50b3d7c1cbceb93689b6269
Moin pasha <79moin@gmail.com>
2026-04-09 16:54:29 +0530

    test: webhook trigger

--- app.py
+++ app.py
@@ -7,7 +7,8 @@ def home():
     return jsonify({
         "status": "running",
         "project": "AutoDeploy360",
-        "author": "Moin Pasha"
+        "author": "Moin Pasha",
+        "version": "2.0"
     })
 
 @app.route('/health')
